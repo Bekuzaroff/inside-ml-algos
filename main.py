1b7b4a9578d3035565f931eeebc3e6b3ee1ad20d
= <bekuzarovchermen@gmail.com>
2026-01-03 20:36:25 +0300

    Commit: gradient descent

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.metrics import mean_squared_error
 
 
 X = np.random.rand(100, 1)
-y = 4 + 3 * X + np.random.rand(100, 1) # so our thetas uqual 4 and 3
+y = 4 + 3 * X + np.random.rand(100, 1) # so our thetas equal 4 and 3
 # plt.plot(X, y, "b.")
 # plt.xlabel("$x_1$", fontsize=18)
 # plt.ylabel("$y$", rotation=0, fontsize=18)
@@ -13,10 +13,12 @@ y = 4 + 3 * X + np.random.rand(100, 1) # so our thetas uqual 4 and 3
 # plt.show()
 
 
-X_b = np.c_[np.ones((100, 1)), X]
-theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y) ##normal equatation
-theta2 = np.linalg.pinv(X_b).dot(y) # SVD
+X_b = np.c_[np.ones((100, 1)), X] # adding x0 attrs to every sample
+theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y) ## normal equatation
+theta2 = np.linalg.pinv(X_b).dot(y) ## SVD
 print(theta, theta2)
+
+# testing
 # print(theta)
 # X_new = np.array([0, 2])
 # X_new_b = np.c_[np.ones((2,1)), X_new]
@@ -27,3 +29,15 @@ print(theta, theta2)
 # y_predict = X_b.dot(theta) 
 # print(mean_squared_error(y, y_predict))
 # print(y[:5], y_predict[:5])
+
+# gradient descent
+eta = 0.1
+n_iters = 1000
+m = 100
+
+theta = np.random.randn(2,1)
+
+for iter in range(n_iters):
+    gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+    theta = theta - eta * gradients
+print(theta)
